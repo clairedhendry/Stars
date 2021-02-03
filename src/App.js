6906bb5ec04c0ad94501e1c576ea8e9f53560fe7
Claire Hendry <claire.d.hendry@gmail.com>
2021-02-03 11:17:31 -0500

    created starfield
    - responds to mouse down and move
    - allows user to draw lines of stars and reset page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import React from 'react';
 import "./App.css";
-// import { useState, useEffect } from "react";
-// import Dot  from './components/dot'
 import Stars from './components/stars'
-
-
-// const useMousePosition = () => {
-//   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
-
-//   const updateMousePosition = ev => {
-//     setMousePosition({ x: ev.clientX, y: ev.clientY });
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("mousemove", updateMousePosition);
-
-//     return () => window.removeEventListener("mousemove", updateMousePosition);
-//   }, []);
-
-//   return mousePosition;
-// };
-
-// const renderDot = () => {
-//   let dot = Dot()
-//   return dot;
-  
-// };
-
-// function App() {
-//   const { x, y } = useMousePosition();
-
-
-//   return (
-
-//     <div className="App" style={{backgroundColor: `rgb(${x}, ${y}, ${y})`}} >
-//       {renderDot}
-//     </div>
-//   );
-// }
-
-// export default App;
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <>
+      <div id="container" className="App">
         <Stars />
       </div>
+     
+      </>
     )
   }
 }
--- src/components/stars.js
+++ src/components/stars.js
@@ -7,6 +7,16 @@ export default class Stars extends React.Component {
   state = {
    stars: [],
    starNumber: 0,
+   isDrawing: false,
+  }
+
+  handleButtonClick = (ev) => {
+    ev.preventDefault();
+    this.setState({
+      stars: [],
+      starNumber: 0,
+      isDrawing: false
+    });
   }
 
     generateStar = (key, x, y, size, colorX, colorY, colorZ)  => {
@@ -28,9 +38,9 @@ export default class Stars extends React.Component {
       return Math.floor(Math.random() * (max - min) + min); 
     }
   
-    useMousePosition = (ev) => {
+    handleMouseDown = (ev) => {
 
-      let number = this.getRandomInt(5, 30);
+      let number = this.getRandomInt(5, 25);
       let colorX = this.getRandomInt(0, 256);
       let colorY = this.getRandomInt(0, 256);
       let colorZ = this.getRandomInt(0, 256);
@@ -42,32 +52,57 @@ export default class Stars extends React.Component {
       this.setState({
         stars: stars,
         starNumber: starNumber + 1, 
+        isDrawing: true
       });
     };
+
+    handleMouseMove = (ev) => {
+      if(!this.state.isDrawing) {
+        return
+      }
+      this.handleMouseDown(ev);
+    }
+
+    handleMouseUp = () => {
+      this.setState({ isDrawing: false });
+    }
 
     renderStarField = () => {
       let starField = this.state.stars.map((star) => { 
      
       return (<div 
-          className="star"
+          className="star pulse"
           key={star.key}
           style={{position: "absolute",       
                 backgroundColor:`rgb(${star.colorX}, ${star.colorY}, ${star.colorZ})`, 
                 height: `${star.size}px`, 
                 width: `${star.size}px`, 
                 left: `${star.x}px`,
-                top: `${star.y}px`}}></div>)
+                top: `${star.y}px`,
+                boxShadow: `0 0 3px 3px rgb(${star.colorX}, ${star.colorY}, ${star.colorZ})`}}></div>)
           })
     return starField;
+    }
+
+    componentDidMount() {
+      document.addEventListener("mouseup", this.handleMouseUp);
+    }
+  
+    componentWillUnmount() {
+      document.removeEventListener("mouseup", this.handleMouseUp);
     }
 
 
     render() {
 
       return (
-        <div id="starfield" className="starfield" onClick={(ev) => this.useMousePosition(ev)}>
+        <>
+
+        <div id="starfield" className="starfield" onMouseDown={(ev) => this.handleMouseDown(ev)} onMouseMove={(ev) => this.handleMouseMove(ev)} >
           {this.renderStarField()}
         </div>
+        <button onClick={(ev) => this.handleButtonClick(ev)}>Reset</button>
+        </>
       )
     }
     
